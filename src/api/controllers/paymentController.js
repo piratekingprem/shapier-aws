092e45aeed92a728cdd686647a3bcdf43415db5f
Premchand <premchandmali001@gmail.com>
2024-06-10 17:58:04 +0530

    try this time

--- src/api/controllers/paymentController.js
+++ src/api/controllers/paymentController.js
@@ -29,10 +29,12 @@ exports.paymentVerification = async (req, res, next) => {
       razorpay_payment_id,
       razorpay_signature,
     } = req.body;
-    console.log(process.env.RAZORPAY_APT_SECRET)
     const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const secreat  = process.env.RAZORPAY_API_SECRET;
+    console.log(secreat);
     const expectedSignature = crypto
-      .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
+      .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
       .update(body.toString())
       .digest("hex");
     console.log("sig recevied", razorpay_signature);
