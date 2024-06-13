edc5d78e4de2572b399cdf81c9c377b3574d619b
Premchand <premchandmali001@gmail.com>
2024-06-13 23:00:11 +0530

    payment model

--- src/api/models/payment.js
+++ src/api/models/payment.js
@@ -30,6 +30,14 @@ exports.store = async (params) => {
         gst_no,
       ]
     );
+    (message = "Error in creating the product brand"),
+      (code = 400),
+      (data = []);
+    if (product_brand.affectedRows) {
+      message = "order creted successfully";
+      code = 201;
+      data = orders;
+    }
   } catch (error) {
     message = error.message;
   }
