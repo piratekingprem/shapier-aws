9376331667d19b2f4ed900cd9e8ccda0352a1211
Premchand <premchandmali001@gmail.com>
2024-06-13 22:51:33 +0530

    payment model

--- src/api/models/payment.js
+++ src/api/models/payment.js
@@ -6,25 +6,30 @@ exports.store = async (params) => {
     data = [];
   try {
     console.log("params", params);
-    console.log("params billing info",params.billingInfo.firstName);
+    console.log("params billing info", params.billingInfo.firstName);
     const payment_status = "complete";
     const total_amount = +params.total_amount;
     const paymentMode = params.paymentMode;
-
-    // const orders = await db.query(
-    //   `INSERT INTO orders (order_id,user_id,total_amount,payment_mode,payment_status,bill_firstName,bil_lastName,bill_mobile,bill_address,bill_pincode,bill_city,bill_state,bill_email,gst_no) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,[params.razorpay_order_id,params.userId]
-    // );
-    // const query = "INSERT INTO payment (order_id, payment_id, signature) VALUES (?, ?, ?)";
-    // const values = [params.orderId, params.paymentId, params.signature];
-
-    // // Assuming db.query is a function to execute SQL queries
-    // const result = await db.query(query, values);
-
-    // if (result.affectedRows > 0) {
-    //   message = "Payment stored successfully";
-    //   code = 200;
-    //   data = { orderId: params.orderId, paymentId: params.paymentId, signature: params.signature };
-    // }
+    const gst_no = params.gst_no ?? null;
+    const orders = await db.query(
+      `INSERT INTO orders (order_id,user_id,total_amount,payment_mode,payment_status,bill_firstName,bil_lastName,bill_mobile,bill_address,bill_pincode,bill_city,bill_state,bill_email,gst_no) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+      [
+        params.razorpay_order_id,
+        params.userId,
+        total_amount,
+        paymentMode,
+        payment_status,
+        params.billingInfo.firstName,
+        params.billingInfo.lastName,
+        params.billingInfo.phone,
+        params.billingInfo.address,
+        params.billingInfo.pincode,
+        params.billingInfo.city,
+        params.billingInfo.state,
+        params.billingInfo.email,
+        gst_no,
+      ]
+    );
   } catch (error) {
     message = error.message;
   }
